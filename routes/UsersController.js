4081d3f12544d6f72c114d4ef89b12ca59d685d7
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-10-15 00:36:38 -0400

    finish user controller

--- routes/UsersController.js
+++ routes/UsersController.js
@@ -25,4 +25,14 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.post('/', async (req, res) => {
+    try {
+        const newUser = new User(req.body.user)
+        const saved = await newUser.save()
+        res.json(saved)
+    } catch (err) {
+        res.send(err)
+    }
+})
+
 module.exports = router
