bce3545826151f159a02f4b6af9e61380249410d
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-10-15 14:48:57 -0400

    finish the sign up form

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 
-//
+//import react 
 import React, { Component } from 'react'
+
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import HomePage from './components/home/HomePage'
-import LoginPage from './components/login/LoginPage'
-import FieldPage from './components/field/FieldPage'
+
+//import the components 
+import HomePage from './components/home/HomePage.js'
+import LoginPage from './components/login/LoginPage.js'
+import FieldPage from './components/field/FieldPage.js'
 
 class App extends Component {
   render() {
@@ -25,7 +28,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route exact path="/login" component={LoginPage} />
-            <Route exact path="/field" component={FieldPage} />
+            <Route exact path="/field:userId" component={FieldPage} />
           </Switch>
         </div>
       </Router>
---
+++ client/src/components/login/SignUpForm.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+
+class SignUpForm extends Component {
+    state = {
+        newUser: {
+            userName: '',
+            password: ''
+        }
+    }
+
+    handleChange = (event) => {
+        const attribute = event.target.name 
+        const updateUser = {...this.state.newUser}
+        updateUser[attribute] = event.target.value
+        this.setState({newUser: updateUser})
+    }
+    handleChange = async (event) => {
+        event.preventDefault()
+        const res = await axios.post('/api/users', {
+            'user': this.state.newUser
+        })
+        console.log(res.data)
+    }
+    render () {
+        return (
+            <div>
+                <h1> Sign Up</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="userName">User Name</label>
+                        <input  
+                        onChange={this.handleChange}
+                        name="userName" type="text"  value={this.state.newUser.userName}/>
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input 
+                        onChange={this.handleChange}
+                        value={this.state.newUser.password}
+                        name="password" type="text" />
+                    </div>
+                    <button> Sign Up</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUpForm
