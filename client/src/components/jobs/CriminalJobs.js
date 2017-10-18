7b8f16a4ee759adb68e8293b5732613029b3466c
Jadenpoole3 <jadenkinpool@yahoo.com>
2017-10-18 17:01:08 -0400

    merging

--- client/src/components/jobs/CriminalJobs.js
+++ client/src/components/jobs/CriminalJobs.js
@@ -2,39 +2,66 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import {Redirect} from 'react-router-dom'
 import styled from 'styled-components'
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 
-const CriminalJobsStyles = styled.div`
-img{
-    height: 200px;
-    width: 40%;
+
+
+
+const styles = {
+    card: {
+        margin: 'auto',
+        width: 650,
+    }
 }
-`
-
-
 
 class CriminalJobs extends Component {
     render() {
         return (
-            <div>
-                <h1> Criminal Justice Jobs</h1>
+            <div style={styles.card} >
 
-                <CriminalJobsStyles>
 
-                <h3> Detectives </h3>
-                <img src="https://fthmb.tqn.com/aoGc6_Quft4LiJ0ndIQNvsMBoBA=/768x0/filters:no_upscale()/108274132-56a1e29d5f9b58b7d0c44037.jpg" alt="detective"/>
-                <p>Salary: $70K - $110K (changes may vary depending on location)</p>
-                <p>School Years: 1-2 </p>
-                <p>Demand: Growing</p>
 
-                <h3> Police Officer </h3>
-                <img src="https://www.drugrehab.com/wp-content/themes/dr/images/police/image-protecting.png" alt="cops"/>
-                <p> Salary: $45K - $70K</p>
-                <p> School Years: 1</p>
-                <p>Demand: High</p>
-                </CriminalJobsStyles>
+<Card>
+                    <CardHeader
+                        title="Criminal Justice Jobs"
+                        subtitle="Detectives"
+                        avatar="https://fthmb.tqn.com/aoGc6_Quft4LiJ0ndIQNvsMBoBA=/768x0/filters:no_upscale()/108274132-56a1e29d5f9b58b7d0c44037.jpg" alt="detective" alt="front end"
+                    />
+                    <CardMedia
+                        overlay={<CardTitle title="Criminal Justice Jobs" subtitle="Detectives" />}
+                    >
+                        <img src="https://fthmb.tqn.com/aoGc6_Quft4LiJ0ndIQNvsMBoBA=/768x0/filters:no_upscale()/108274132-56a1e29d5f9b58b7d0c44037.jpg" alt="detective" alt="front end" alt="nurse" />
+                    </CardMedia>
+                    <CardTitle title="Criminal Justice Jobs" subtitle="Detectives " />
+                    <CardText>
+                    Salary: $70K - $110K (changes may vary depending on location)
+                    School Years: 1-2
+                    Demand: Growing
+    </CardText>
+         
+                </Card>
 
-                
 
+                <Card>
+                    <CardHeader
+                        title="Criminal Justice Jobs"
+                        subtitle="Police Officer "
+                        avatar="https://www.drugrehab.com/wp-content/themes/dr/images/police/image-protecting.png"  alt="detective" alt="front end"
+                    />
+                    <CardMedia
+                        overlay={<CardTitle title="Criminal Justice Jobs" subtitle="Police Officer " />}
+                    >
+                        <img src="https://www.drugrehab.com/wp-content/themes/dr/images/police/image-protecting.png"  alt="detective" alt="front end" alt="nurse" />
+                    </CardMedia>
+                    <CardTitle title="Criminal Justice Jobs" subtitle="Police Officer  " />
+                    <CardText>
+                    Salary: $45K - $70K (changes may vary depending on location)
+                    School Years: 1
+                    Demand: High
+
+    </CardText>
+                </Card>
             </div>
         );
     }
--- client/src/components/jobs/MedicalJob.js
+++ client/src/components/jobs/MedicalJob.js
@@ -70,29 +70,6 @@ class MedicalJob extends Component {
     </CardActions> */}
   </Card>
 
-
-{/* 
-                <h1> Medical Jobs</h1>
-                
-                <h3> Nurse Anesthetist </h3>
-                <img src="http://nursinglink.monster.com/nfs/nursinglink/attachment_images/0015/8190/anesthetistnurse_crop380w.jpg?1284503282" alt="nurse anthe"/>
-                <p> Salary: $100K - $150K (changes may vary depending on location)</p>
-                <p> School Years: Two - Six </p>
-                <p>Demand: High</p>
-
-                <br/>
-
-                <h3> Surgical Technical </h3>
-                <img src="https://www.allalliedhealthschools.com/wp-content/uploads/2016/09/hero-medical-technician-surgical-assistant-vs-technician.jpg" alt="nurse surge"/>
-                <p> Salary: $50K - $90K (changes may vary depending on location)</p>
-                <p> School Years: Two - Four</p>
-                Demand: Skyrocketing */}
-                {/* </MedicalJobStyles> */}
-
-
-
-              
-
             </div>
         );
     }
